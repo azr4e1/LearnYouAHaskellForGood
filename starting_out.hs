2d3b8d10968992ba528911bd2b1c353f717c6e42
Lorenzo Drumond <drumondlorenzo@posteo.net>
2023-06-10 04:07:10 +0100

    chore: commit save point

---
+++ starting_out.hs
@@ -0,0 +1,34 @@
+-- Arithmetic
+2 + 15
+49 * 100
+1892 - 1472
+5 / 2
+(50 * 100) - 4999
+50 * 100 - 4999
+50 * (100 - 4999)
+
+-- Boolean
+True && False
+True && True
+False || True
+not False
+not (True && True)
+
+-- Equality
+5 == 5
+1 == 0
+5 /= 5
+5 /= 4
+"hello" == "hello"
+
+5 + "llama"
+5 == True
+
+-- Functions
+succ 8
+min 9 10
+max 100 101
+-- Function application has the highest precedence
+succ 9 + max 5 4 + 1
+-- same as
+(succ 9) + (max 5 4) + 1
