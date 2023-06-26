8794552e760eafc7377c9e690b5619c1d6b2d074
Lorenzo Drumond <drumondlorenzo@posteo.net>
2023-06-26 02:32:30 +0100

    chore: commit save point

---
+++ recursion.hs
@@ -0,0 +1,60 @@
+-- Recursion is actually a way of defining functions in which the function is applied inside its own definition. Definitions in mathematics are often given recursively.
+-- Having an element or two in a recursion definition defined non-recursivelyis also called the edge condition and is important if you want your recursive function to terminate.
+-- Recursion is important to Haskell because unlike imperative languages, you do computations in Haskell by declaring what something is instead of declaring how you get it. That's why there are no while loops or for loops in Haskell and instead we many times have to use recursion to declare what something is.
+--
+-- Maximum algorithm
+-- in imperative fashion (python):
+-- curr_max = 0
+-- for i in my_list:
+--     if i > curr_max:
+--         curr_max = i
+-- 
+-- in Haskell:
+maximum' :: (Ord a) => [a]  -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = maximum' xs
+
+maximum'' :: (Ord a) => [a] -> a
+maximum'' [] = error "maximum of empty list"
+maximum'' [x] = x
+maximum'' (x:xs) = max x (maximum'' xs)
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0    = []
+    | otherwise = x:replicate' (n-1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0        = []
+take' _ []          = []
+take' n (x:xs)      = x:take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y):zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+    | a == x    = True
+    | otherwise = a `elem'` xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = 
+    let smallerSorted = quicksort [a | a <- xs, a <= x]
+        biggerSorted = quicksort [a | a <- xs, a > x]
+    in smallerSorted ++ [x] ++ biggerSorted
